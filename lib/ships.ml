83e2b0504775d4fafe6f7b030230b0f8e66290c2
Amelia Zheng <ayz23@cornell.edu>
2024-04-12 17:16:20 -0400

    Change the user input coordinates to make it easier for the user
    - implemented functionality for allowing the user to chose the orientation

--- lib/ships.ml
+++ lib/ships.ml
@@ -8,7 +8,7 @@ type ship = {
 (* type of guesses*)
 type guesses = (int * char) list
 
-let coord_add coord str str2 = (str, str2) :: coord
+let coord_add coord lst = lst :: coord
 let create_ship name length a = { name; length; coordinates = a; hits = 0 }
 
 (**let output match_emoji = function | true -> "❌" | false -> "⚪️" **)
